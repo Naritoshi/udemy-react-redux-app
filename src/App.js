270805e760b5dad14db394471f51796ac0d27db0
Naritoshi <naritoshi.matsuo@gmail.com>
2019-04-13 12:23:27 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
+// class App extends Component {
+//   render() {
+//     return <div>Hello World!!</div>;
+//   }
+// }
 class App extends Component {
   render() {
-    return <div><h1>Hello World.</h1></div>
+    return React.createElement(
+      "h1",
+      null,
+      "Hello world!"
+    );
   }
 }
-
 export default App;
